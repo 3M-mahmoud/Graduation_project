3fdbf9b91ad9f8ee46c6f7a5eb50c807a1637ddc
mahmoud <darbakmostafa25@gmail.com>
2026-05-09 20:39:53 +0300

    update date & fix filter some page

--- src/app/components/profile/ProfileHeader.tsx
+++ src/app/components/profile/ProfileHeader.tsx
@@ -12,13 +12,8 @@ export const ProfileHeader = ({ data }: any) => {
   }, [data]);
 
   return (
-    <div className="relative bg-white shadow-sm pb-4">
-      <div className="h-48 md:h-72 w-full overflow-hidden">
-        {/* <Image
-          src={dataHeader?.imageUrl || ""}
-          alt="Cover"
-          className="w-full h-full object-cover"
-        /> */}
+    <div className="relative bg-white shadow-sm pb-6">
+      <div className="relative h-48 md:h-72 overflow-hidden">
         {dataHeader?.imageUrl && (
           <Image
             src={dataHeader?.imageUrl}
@@ -33,16 +28,12 @@ export const ProfileHeader = ({ data }: any) => {
       <div className="max-w-7xl mx-auto px-6 relative">
         <div className="flex flex-col md:flex-row items-center md:items-end gap-6 -mt-16 md:-mt-20">
           <div data-aos="zoom-in" className="relative group">
-            <div className="w-32 h-32 md:w-44 md:h-44 rounded-full border-4 border-white bg-white shadow-md z-20">
-              {dataHeader?.imageUrl && (
-                <Image
-                  src={dataHeader?.imageUrl}
-                  alt={dataHeader?.name}
-                  fill
-                  className="object-cover w-32 h-32 rounded-full"
-                  unoptimized
-                />
-              )}
+            <div className="w-32 h-32 md:w-44 md:h-44 rounded-full border-4 border-white bg-white shadow-md overflow-hidden z-20">
+              <Image
+                src={dataHeader?.imageUrl || ""}
+                alt="Center Logo"
+                className="w-full h-full object-cover"
+              />
             </div>
           </div>
 
